8987aa17277e23a65a8760479ddea4b5a0d1f9bf
JiHun Hong <hongji3354@gmail.com>
2022-09-03 22:40:07 +0900

    feat : MenuGroup 생성자 추가

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -4,14 +4,19 @@ public class MenuGroup {
     private Long id;
     private String name;
 
-    public MenuGroup() {
-    }
-
-    public MenuGroup(final String name) {
+    public MenuGroup(final Long id, final String name) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("메뉴 그룹의 이름이 존재해야 합니다.");
         }
+        this.id = id;
         this.name = name;
+    }
+
+    public MenuGroup(final String name) {
+        this(null, name);
+    }
+
+    public MenuGroup() {
     }
 
     public Long getId() {
